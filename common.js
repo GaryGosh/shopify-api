52b158c36da531a15505c33e7d58b24aa0b51e5e
Neeraj TK <64883030+GaryGosh@users.noreply.github.com>
2022-12-02 15:21:46 +0530

    added products api to fetch in common js

--- common.js
+++ common.js
@@ -25,3 +25,29 @@ export async function getShopifyOrderDetails() {
     });
   });
 }
+
+export async function getShopifyProductDetails() {
+  const options = {
+    method: "GET",
+    url: `https://${process.env.API_KEY}:${process.env.ADMIN_API}@${process.env.SHOP}/admin/api/2022-10/product.json`,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  return new Promise((resolve, reject) => {
+    request(options, (error, response) => {
+      if (!error) {
+        let responseBody = response.body;
+        responseBody = JSON.parse(responseBody);
+        resolve(responseBody);
+      } else {
+        reject({
+          error: {
+            statusCode: 500,
+            body: { error: "Something went wrong with shopify server" },
+          },
+        });
+      }
+    });
+  });
+}
